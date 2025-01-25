438ade3b8e9a8f2484804cdd2793c85b9967de0b
Srikant <srikantddazzler387@gmail.com>
2025-01-25 17:46:36 +0530

    Final commit 1

--- project_blog/settings.py
+++ project_blog/settings.py
@@ -137,7 +137,12 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 MEDIA_ROOT = BASE_DIR / "uploads"
